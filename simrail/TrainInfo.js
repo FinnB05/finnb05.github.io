3c9dc0ff2f3e67a44fe5d6f30c8f4f306ed59d98
Femke <finn.beumer@hotmail.com>
2024-01-01 00:14:06 +0100

    Speed

---
+++ simrail/TrainInfo.js
@@ -0,0 +1,37 @@
+const search = new URLSearchParams(window.location.search);
+let trainNumber = null;
+
+if (search.has('train')) {
+    trainNumber = search.get('train');
+}
+
+if (trainNumber) {
+    getTrainData();
+}
+
+function getTrainData() {
+    $.ajax({
+        url: `https://panel.simrail.eu:8084/trains-open?serverCode=en1`,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            const trains = data.data;
+            //console.log(trains);
+
+            for(let i = 0; i < trains.length; i++) {
+
+                if(trains[i].TrainNoLocal == trainNumber) {
+                    console.log(trains[i]);
+                    document.getElementById("speed").innerText = Math.round(parseInt(trains[i].TrainData.Velocity));
+                }
+
+            }
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    })
+
+    setTimeout(getTrainData, 2500);
+
+}
